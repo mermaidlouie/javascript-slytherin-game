956f84223115431baffe2dc4bcaf03db7b483223
Vique <victoria.ramiro@gmail.com>
2020-05-18 13:16:04 -0300

    jogo pronto

--- script.js
+++ script.js
@@ -1,73 +1,75 @@
-let canvas = document.getElementById("snake");
-let context = canvas.getContext("2d");
-let box = 32;
+const canvas = document.querySelector('#snake');
+const começar = document.querySelector('#comecar');
+const context = canvas.getContext('2d');
+const box = 32;
 let snake = [];
 snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
-let direction = "right";
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
     y: Math.floor(Math.random() * 15 + 1) * box
 }
 
+let direction = 'right';
 
-function createBG() {
-    context.fillStyle = "black";
+function criarBg() {
+    context.fillStyle = 'black';
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function createCobrinha() {
-    for (i = 0; i < snake.length; i++) {
-        context.fillStyle = "pink";
+function criarSnake() {
+    for (i=0; i < snake.length; i++) {
+        context.fillStyle = 'pink';
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
 function drawFood() {
-    context.fillStyle = "blue";
+    context.fillStyle = 'blue';
     context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
 
 function update(event) {
-    if (event.keyCode == 37 && direction != "right") direction = "left";
-    if (event.keyCode == 38 && direction != "down") direction = "up";
-    if (event.keyCode == 39 && direction != "left") direction = "right";
-    if (event.keyCode == 40 && direction != "up") direction = "down";
+    if(event.keyCode === 37 && event.keyCode !== 'right') direction = 'left';
+    if(event.keyCode === 38 && event.keyCode !== 'down') direction = 'up';
+    if(event.keyCode === 39 && event.keyCode !== 'left') direction = 'right';
+    if(event.keyCode === 40 && event.keyCode !== 'up') direction = 'down';
 
+    console.log(direction);
 }
 
-
 function iniciarJogo() {
-
-    if (snake[0].x > 15 * box && direction === 'right') snake[0].x = 0;
-    if (snake[0].x < 0 && direction === 'left') snake[0].x = 16 * box;
-    if (snake[0].y > 15 * box && direction === 'down') snake[0].y = 0;
-    if (snake[0].y < 0 && direction === 'up') snake[0].y = 16 * box;
-
+    if(snake[0].x > 15 * box && direction === 'right') snake[0].x = 0;
+    if(snake[0].x < 0 && direction === 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction === 'down') snake[0].y = 0;
+    if(snake[0].y < 0 && direction === 'up') snake[0].y = 16 * box;
 
     for(i = 1; i < snake.length; i++) {
         if(snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
             clearInterval(jogo);
             comecar.removeAttribute('disabled');
-            alert('Game Over :(');
+            alert('Game Over! Recarregue a página.');
         } 
     }
-
-    createBG();
-    createCobrinha();
+    
+    criarBg();
+    criarSnake();
     drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if (direction == "right") snakeX += box;
-    if (direction == "left") snakeX -= box;
-    if (direction == "up") snakeY -= box;
-    if (direction == "down") snakeY += box;
+    switch (direction) {
+        case 'right': snakeX += box; break;
+        case 'left': snakeX -= box; break;
+        case 'up': snakeY -= box; break;
+        case 'down': snakeY += box; break;
+        default: break;       
+    }
 
     if (snakeX !== food.x || snakeY !== food.y) {
         snake.pop();
@@ -76,13 +78,30 @@ function iniciarJogo() {
         food.y = Math.floor(Math.random() * 15 + 1) * box;
     }
 
-    let newHead = {
+    const newHead = {
         x: snakeX,
         y: snakeY
     }
 
     snake.unshift(newHead);
-
 }
 
-let jogo = setInterval(iniciarJogo, 100);
+function resetGame() {
+    snake = [];
+    snake[0] = {
+        x: 8 * box,
+        y: 8 * box
+    };
+
+    food = {
+        x: Math.floor(Math.random() * 15 + 1) * box,
+        y: Math.floor(Math.random() * 15 + 1) * box
+    };
+}
+
+let jogo = '';
+comecar.onclick = () => {
+    resetGame();
+    comecar.setAttribute('disabled', 'disabled');
+    jogo = setInterval(iniciarJogo, 100);
+}
